181b1d99695442e611f4829470c86e5b3381179d
AJ <alexanderjessediaz@gmail.com>
2021-09-30 15:56:16 -0600

    add Entypo icons and a new color to constants

--- App/screens/options.js
+++ App/screens/options.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import { TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native'
+import {Entypo} from '@expo/vector-icons'
 import colors from '../constants/colors'
 
 
 const styles = StyleSheet.create({
     row: {
         paddingVertical:16,
-        paddingHorizontal:20
+        paddingHorizontal:20,
+        flexDirection: 'row',
+        justifyContent: "space-between"
     },
     text: {
         fontSize:16,
@@ -25,12 +28,15 @@ export default () => {
       <SafeAreaView>
         <TouchableOpacity style={styles.row}>
           <Text styles={styles.text}>Themes</Text>
+          <Entypo name="chevron-right" size={20} color={colors.blue} />
         </TouchableOpacity>
         <TouchableOpacity style={styles.row}>
           <Text styles={styles.text}>React Native Basics</Text>
+          <Entypo name="export" size={20} color={colors.blue} />
         </TouchableOpacity>
         <TouchableOpacity style={styles.row}>
           <Text styles={styles.text}>React Native by Example</Text>
+          <Entypo name="export" size={20} color={colors.blue} />
         </TouchableOpacity>
       </SafeAreaView>
     )
